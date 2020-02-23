f67c19a0f625de5b611bb5123794a626c5ef7b4e
hani <1944237687@qq.com>
2020-02-23 22:38:00 +0800

    200223

--- style.js
+++ style.js
@@ -2,12 +2,17 @@ let navStatus = false;
 
 let menu = function(){
     let getSidebar = document.querySelector(".main-nav");
+    let input = document.querySelector(".input")
     if (navStatus === false){
         getSidebar.style.display = "block";
+        input.style.left = "380px";
+        input.style.width = "calc(100vw - 380px)";
         navStatus = true
     }
     else if (navStatus === true){
         getSidebar.style.display = "none";
+        input.style.left = "0";
+        input.style.width = "100%";
         navStatus = false
     }
 }
