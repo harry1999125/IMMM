8fa4a1a2fd7409667a859c572651a744521875d6
hani <1944237687@qq.com>
2020-02-25 22:15:42 +0800

    back

--- style.js
+++ style.js
@@ -1,18 +1,34 @@
+let getSidebar = document.querySelector(".main-nav");
+let input = document.querySelector(".input");
+let output = document.querySelector(".output");
+
 let navStatus = false;
 
 let menu = function(){
-    let getSidebar = document.querySelector(".main-nav");
-    let input = document.querySelector(".input")
+
     if (navStatus === false){
         getSidebar.style.display = "block";
         input.style.left = "260px";
         input.style.width = "calc(100vw - 260px)";
-        navStatus = true
+        output.style.left = "260px";
+        output.style.width = "calc(100vw - 260px)";
+        navStatus = true;
     }
     else if (navStatus === true){
         getSidebar.style.display = "none";
         input.style.left = "0";
         input.style.width = "100%";
-        navStatus = false
+        output.style.left = "0";
+        output.style.width = "100%";
+        navStatus = false;
     }
+}
+
+let submit = function(){
+    input.style.display = "none";
+    output.style.display = "block";
+}
+let backToInput = function(){
+    input.style.display = "block";
+    output.style.display = "none";
 }
